0e53fb19c9544be3e3bad917d74185cadad85fa7
Barna Nagy <barna@meetmarkko.com>
2025-01-31 10:44:21 +0000

    update blogs api to handle external token data

--- src/api/blogs.ts
+++ src/api/blogs.ts
@@ -1,5 +1,5 @@
 import { BaseAPI } from './base';
-import { AuthService } from '../services/auth';
+import { AuthService, TokenData } from '../services/auth';
 
 export class BlogsAPI extends BaseAPI {
   constructor(config: any, authService: AuthService) {
@@ -9,25 +9,43 @@ export class BlogsAPI extends BaseAPI {
   /**
    * Get a list of blog posts
    * @param params - The query parameters to filter the blog posts
+   * @param oauth - The OAuth token data
    * @returns A list of blog posts
    */
-  async listPosts(params = {}) {
+  async listPosts(params = {}, oauth: TokenData | null = null) {
     const url = `${this.config.apiBasePath}/api/v1/blog/posts`;
-    const response = await this.axiosInstance.get(url, { params });
+    const config: any = { params };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.get(url, config);
     return response.data;
   }
 
   /**
    * Get a list of blog categories
    * @param params - The query parameters to filter the blog categories
+   * @param oauth - The OAuth token data
    * @returns A list of blog categories
    */
-  async listCategories(params = {}) {
+  async listCategories(params = {}, oauth: TokenData | null = null) {
     const defaultParams = {
       is_active: 1
     };
     const url = `${this.config.apiBasePath}/api/v1/blog/categories`;
-    const response = await this.axiosInstance.get(url, { params: { ...defaultParams, ...params } });
+    const config: any = { params: { ...defaultParams, ...params } };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.get(url, config);
     return response.data;
   }
 
@@ -35,11 +53,20 @@ export class BlogsAPI extends BaseAPI {
    * Get a list of posts by blog category
    * @param categoryId - The ID of the blog category
    * @param params - The query parameters to filter the blog posts
+   * @param oauth - The OAuth token data
    * @returns A list of blog posts
    */
-  async listPostsByCategory(categoryId: number, params = {}) {
+  async listPostsByCategory(categoryId: number, params = {}, oauth: TokenData | null = null) {
     const url = `${this.config.apiBasePath}/api/v1/blog/categories/${categoryId}`;
-    const response = await this.axiosInstance.get(url, { params });
+    const config: any = { params };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.get(url, config);
     return response.data;
   }
 
@@ -47,11 +74,20 @@ export class BlogsAPI extends BaseAPI {
    * Get a single blog post
    * @param id - The ID of the blog post
    * @param params - The query parameters to filter the blog post
+   * @param oauth - The OAuth token data
    * @returns A single blog post
    */
-  async getPost(id: number, params = {}) {
+  async getPost(id: number, params = {}, oauth: TokenData | null = null) {
     const url = `${this.config.apiBasePath}/api/v1/blog/posts/${id}`;
-    const response = await this.axiosInstance.get(url, { params });
+    const config: any = { params };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.get(url, config);
     return response.data;
   }
 
@@ -59,11 +95,20 @@ export class BlogsAPI extends BaseAPI {
    * Get a single blog post by slug
    * @param slug - The slug of the blog post
    * @param params - The query parameters to filter the blog post
+   * @param oauth - The OAuth token data
    * @returns A single blog post
    */
-  async getPostBySlug(slug: string, params = {}) {
+  async getPostBySlug(slug: string, params = {}, oauth: TokenData | null = null) {
     const url = `${this.config.apiBasePath}/api/v1/blog/posts/slug/${slug}`;
-    const response = await this.axiosInstance.get(url, { params });
+    const config: any = { params };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.get(url, config);
     return response.data;
   }
 
@@ -71,14 +116,23 @@ export class BlogsAPI extends BaseAPI {
    * Get a blog category by slug with its posts
    * @param slug - The slug of the blog category
    * @param params - The query parameters to filter the blog category
+   * @param oauth - The OAuth token data
    * @returns A single blog category with its posts
    */
-  async getCategoryBySlug(slug: string, params = {}) {
+  async getCategoryBySlug(slug: string, params = {}, oauth: TokenData | null = null) {
     const defaultParams = {
       is_active: 1
     };
     const url = `${this.config.apiBasePath}/api/v1/blog/categories/slug/${slug}`;
-    const response = await this.axiosInstance.get(url, { params: { ...defaultParams, ...params } });
+    const config: any = { params: { ...defaultParams, ...params } };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.get(url, config);
     return response.data;
   }
 }
