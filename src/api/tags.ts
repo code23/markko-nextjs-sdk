b75b42478212d4ba696a0af69e45989158a3c8c9
Barna Nagy <barna@meetmarkko.com>
2025-01-31 10:45:26 +0000

    upate tags api to handle external token data

--- src/api/tags.ts
+++ src/api/tags.ts
@@ -1,5 +1,5 @@
 import { BaseAPI } from './base';
-import { AuthService } from '../services/auth';
+import { AuthService, TokenData } from '../services/auth';
 
 export class TagsAPI extends BaseAPI {
   constructor(config: any, authService: AuthService) {
@@ -9,12 +9,21 @@ export class TagsAPI extends BaseAPI {
   /**
    * Get a list of tags
    * @param params - The query parameters to filter the tags
+   * @param oauth - The OAuth token data
    * @returns A list of tags
    */
-  async list(params = {}) {
+  async list(params = {}, oauth: TokenData | null = null) {
     const url = `${this.config.apiBasePath}/api/v1/tags`;
+    const config: any = { params };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
     try {
-      const response = await this.axiosInstance.get(url, { params });
+      const response = await this.axiosInstance.get(url, config);
       return response.data;
     } catch (error) {
       throw new Error("A problem was encountered during the tags retrieval.");
