cf9f940c16000062396c704b44353f8aa2997783
Barna Nagy <barna@meetmarkko.com>
2025-01-31 10:45:45 +0000

    update vendors api to handle external token data

--- src/api/vendors.ts
+++ src/api/vendors.ts
@@ -1,5 +1,5 @@
 import { BaseAPI } from './base';
-import { AuthService } from '../services/auth';
+import { AuthService, TokenData } from '../services/auth';
 import { APIError } from '../types';
 
 export class VendorsAPI extends BaseAPI {
@@ -10,11 +10,20 @@ export class VendorsAPI extends BaseAPI {
   /**
    * Get a list of vendors
    * @param params - The query parameters to filter the vendors
+   * @param oauth - The OAuth token data
    * @returns A list of vendors
    */
-  async list(params = {}) {
+  async list(params = {}, oauth: TokenData | null = null) {
     const url = `${this.config.apiBasePath}/api/v1/vendors`;
-    const response = await this.axiosInstance.get(url, { params });
+    const config: any = { params };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.get(url, config);
     return response.data;
   }
 
@@ -22,15 +31,24 @@ export class VendorsAPI extends BaseAPI {
    * Get a list of vendors by postcode
    * @param postcode - The postcode to filter the vendors
    * @param params - The query parameters to filter the vendors
+   * @param oauth - The OAuth token data
    * @returns A list of vendors
    */
-  async listByPostcode(postcode: string, params = {}) {
+  async listByPostcode(postcode: string, params = {}, oauth: TokenData | null = null) {
     const defaultParams = {
       is_active: 1
     };
     postcode = postcode.replace(/\s+/g, '');
     const url = `${this.config.apiBasePath}/api/v1/vendors/postcode/${postcode}`;
-    const response = await this.axiosInstance.get(url, { params: { ...defaultParams, ...params } });
+    const config: any = { params: { ...defaultParams, ...params } };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.get(url, config);
     return response.data;
   }
 
@@ -38,11 +56,20 @@ export class VendorsAPI extends BaseAPI {
    * Get a single vendor
    * @param id - The ID of the vendor
    * @param params - The query parameters to filter the vendor
+   * @param oauth - The OAuth token data
    * @returns A single vendor
    */
-  async get(id: number, params = {}) {
+  async get(id: number, params = {}, oauth: TokenData | null = null) {
     const url = `${this.config.apiBasePath}/api/v1/vendors/${id}`;
-    const response = await this.axiosInstance.get(url, { params });
+    const config: any = { params };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.get(url, config);
     return response.data;
   }
 
@@ -50,27 +77,45 @@ export class VendorsAPI extends BaseAPI {
    * Get a single vendor by slug
    * @param slug - The slug of the vendor
    * @param params - The query parameters to filter the vendor
+   * @param oauth - The OAuth token data
    * @returns A single vendor
    */
-  async getBySlug(slug: string, params = {}) {
+  async getBySlug(slug: string, params = {}, oauth: TokenData | null = null) {
     const defaultParams = {
       is_active: 1
     };
+    const config: any = { params: { ...defaultParams, ...params } };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
     const url = `${this.config.apiBasePath}/api/v1/vendors/slug/${slug}`;
-    const response = await this.axiosInstance.get(url, { params: { ...defaultParams, ...params } });
+    const response = await this.axiosInstance.get(url, config);
     return response.data;
   }
 
   /**
    * Save a new vendor
    * @param data The vendor data to be saved
+   * @param oauth - The OAuth token data
    * @throws {APIError} If the API request fails or returns an error
    * @returns Promise<boolean> Returns true if successful
    */
-  async save(data: Record<string, any>): Promise<boolean> {
+  async save(data: Record<string, any>, oauth: TokenData | null = null): Promise<boolean> {
     try {
       const url = `${this.config.apiBasePath}/api/v1/vendors/register`;
-      const response = await this.axiosInstance.post(url, data);
+      const config: any = { data };
+
+      if (oauth) {
+        config.headers = {
+          'X-OAuth-Token': JSON.stringify(oauth)
+        };
+      }
+
+      const response = await this.axiosInstance.post(url, data, config);
 
       // Check if the response contains an error
       if (response.data?.error) {
@@ -93,33 +138,60 @@ export class VendorsAPI extends BaseAPI {
   /**
    * Follow a vendor
    * @param id - The ID of the vendor
+   * @param oauth - The OAuth token data
    * @returns A single vendor
    */
-  async follow(id: number) {
+  async follow(id: number, oauth: TokenData | null = null) {
     const url = `${this.config.apiBasePath}/api/v1/vendors/${id}/follow`;
-    const response = await this.axiosInstance.patch(url);
+    const config: any = {};
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.patch(url, config);
     return response.data;
   }
 
   /**
    * Unfollow a vendor
    * @param id - The ID of the vendor
+   * @param oauth - The OAuth token data
    * @returns A single vendor
    */
-  async unfollow(id: number) {
+  async unfollow(id: number, oauth: TokenData | null = null) {
     const url = `${this.config.apiBasePath}/api/v1/vendors/${id}/unfollow`;
-    const response = await this.axiosInstance.patch(url);
+    const config: any = {};
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.patch(url, config);
     return response.data;
   }
 
   /**
    * Check if a vendor (store) name is unique
    * @param name - The vendor (store) name to check
+   * @param oauth - The OAuth token data
    * @returns A boolean indicating if the vendor (store) name is unique
    */
-  async isStoreNameUnique(name: string) {
+  async isStoreNameUnique(name: string, oauth: TokenData | null = null) {
     const url = `${this.config.apiBasePath}/api/v1/vendors/is-store-name-unique`;
-    const response = await this.axiosInstance.get(url, { params: { store_name: name } });
+    const config: any = { params: { store_name: name } };
+
+    if (oauth) {
+      config.headers = {
+        'X-OAuth-Token': JSON.stringify(oauth)
+      };
+    }
+
+    const response = await this.axiosInstance.get(url, config);
     return response.data;
   }
 }
