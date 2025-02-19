6c7152197daa90d43fbb4874465be2ba88201226
Shubhh2903 <shubhamlilawala691@gmail.com>
2025-02-19 16:36:40 +0530

    task: change error handling in charities api

--- src/api/charities.ts
+++ src/api/charities.ts
@@ -1,6 +1,6 @@
-import { BaseAPI } from './base';
-import { AuthService, TokenData } from '../services/auth';
-import { APIError } from '../types';
+import { BaseAPI } from "./base";
+import { AuthService, TokenData } from "../services/auth";
+import { APIError } from "../types";
 
 export class CharitiesAPI extends BaseAPI {
   constructor(config: any, authService: AuthService) {
@@ -14,20 +14,39 @@ export class CharitiesAPI extends BaseAPI {
    * @returns A list of charities
    */
   async list(params = {}, oauth: TokenData | null = null) {
-    const url = `${this.config.apiBasePath}/api/v1/charities`;
-    const config: any = { params };
-
-    if (oauth) {
-      config.headers = {
-        'X-OAuth-Token': JSON.stringify(oauth)
-      };
-    }
-
     try {
+      const url = `${this.config.apiBasePath}/api/v1/charities`;
+      const config: any = { params };
+
+      if (oauth) {
+        config.headers = {
+          "X-OAuth-Token": JSON.stringify(oauth),
+        };
+      }
+
       const response = await this.axiosInstance.get(url, config);
+
+      if (response.data?.error) {
+        throw new APIError(
+          response.data.message,
+          response.data.code,
+          response.data.errors
+        );
+      }
       return response.data;
-    } catch (error) {
-      throw new Error("A problem was encountered during the charities retrieval.");
+    } catch (error: any) {
+      if (error.response?.data) {
+        throw new APIError(
+          error.response.data.message,
+          error.response.status,
+          error.response.data.errors
+        );
+      }
+
+      throw new APIError(
+        "A problem was encountered during the request to fetch list of charities.",
+        422
+      );
     }
   }
 
@@ -39,17 +58,40 @@ export class CharitiesAPI extends BaseAPI {
    * @returns - A promise that resolves to the details of the specified charity.
    */
   async get(id: number, params = {}, oauth: TokenData | null = null) {
-    const url = `${this.config.apiBasePath}/api/v1/charities/${id}`;
-    const config: any = { params };
+    try {
+      const url = `${this.config.apiBasePath}/api/v1/charities/${id}`;
+      const config: any = { params };
 
-    if (oauth) {
-      config.headers = {
-        'X-OAuth-Token': JSON.stringify(oauth)
-      };
+      if (oauth) {
+        config.headers = {
+          "X-OAuth-Token": JSON.stringify(oauth),
+        };
+      }
+
+      const response = await this.axiosInstance.get(url, config);
+      if (response.data?.error) {
+        throw new APIError(
+          response.data.message,
+          response.data.code,
+          response.data.errors
+        );
+      }
+
+      return response.data;
+    } catch (error: any) {
+      if (error.response?.data) {
+        throw new APIError(
+          error.response.data.message,
+          error.response.status,
+          error.response.data.errors
+        );
+      }
+
+      throw new APIError(
+        "A problem was encountered during the request to fetch charity by its id.",
+        422
+      );
     }
-
-    const response = await this.axiosInstance.get(url, config);
-    return response.data;
   }
 
   /**
@@ -60,75 +102,133 @@ export class CharitiesAPI extends BaseAPI {
    * @returns A single charity
    */
   async getBySlug(slug: string, params = {}, oauth: TokenData | null = null) {
-    const defaultParams = {
-      is_active: 1
-    };
-    const url = `${this.config.apiBasePath}/api/v1/charities/slug/${slug}`;
-    const config: any = { params: { ...defaultParams, ...params } };
-
-    if (oauth) {
-      config.headers = {
-        'X-OAuth-Token': JSON.stringify(oauth)
+    try {
+      const defaultParams = {
+        is_active: 1,
       };
-    }
+      const url = `${this.config.apiBasePath}/api/v1/charities/slug/${slug}`;
+      const config: any = { params: { ...defaultParams, ...params } };
 
-    const response = await this.axiosInstance.get(url, config);
-    return response.data;
+      if (oauth) {
+        config.headers = {
+          "X-OAuth-Token": JSON.stringify(oauth),
+        };
+      }
+
+      const response = await this.axiosInstance.get(url, config);
+
+      if (response.data?.error) {
+        throw new APIError(
+          response.data.message,
+          response.data.code,
+          response.data.errors
+        );
+      }
+      return response.data;
+    } catch (error: any) {
+      if (error.response?.data) {
+        throw new APIError(
+          error.response.data.message,
+          error.response.status,
+          error.response.data.errors
+        );
+      }
+
+      throw new APIError(
+        "A problem was encountered during the request to fetch single charity by slug.",
+        422
+      );
+    }
   }
 
-   /**
+  /**
    * Save a new charity
    * @param data The charity data to be saved
    * @param oauth - The OAuth token data
    * @throws {APIError} If the API request fails or returns an error
    * @returns Promise<boolean> Returns true if successful
    */
-   async save(data: Record<string, any>, oauth: TokenData | null = null): Promise<boolean> {
-    const config: any = {};
-
-    if (oauth) {
-      config.headers = {
-        'X-OAuth-Token': JSON.stringify(oauth)
-      };
-    }
-
+  async save(
+    data: Record<string, any>,
+    oauth: TokenData | null = null
+  ): Promise<boolean> {
     try {
       const url = `${this.config.apiBasePath}/api/v1/charities/register`;
-      const response = await this.axiosInstance.post(url, data, config);
-      // Check if the response contains an error
-      if (response.data?.error) {
-        throw new APIError(response.data.message, response.data.code);
+      const config: any = {};
+
+      if (oauth) {
+        config.headers = {
+          "X-OAuth-Token": JSON.stringify(oauth),
+        };
       }
-      return true;
-    } catch (error) {
-      if (error instanceof APIError) {
-        throw error;
-      } else {
+
+      const response = await this.axiosInstance.post(url, data, config);
+
+      if (response.data?.error) {
         throw new APIError(
-          'A problem was encountered during the request to create a new Charity.',
-          422
+          response.data.message,
+          response.data.code,
+          response.data.errors
         );
       }
+
+      return true;
+    } catch (error: any) {
+      if (error.response?.data) {
+        throw new APIError(
+          error.response.data.message,
+          error.response.status,
+          error.response.data.errors
+        );
+      }
+
+      throw new APIError(
+        "A problem was encountered during the request to save a new charity.",
+        422
+      );
     }
   }
 
-   /**
+  /**
    * Check if a charity (store) name is unique
    * @param name - The charity (store) name to check
    * @param oauth - The OAuth token data
    * @returns A boolean indicating if the charity (store) name is unique
    */
   async nameIsUnique(name: string, oauth: TokenData | null = null) {
-    const url = `${this.config.apiBasePath}/api/v1/charities/is-name-unique`;
-    const config: any = { params: { name } };
+    try {
+      const url = `${this.config.apiBasePath}/api/v1/charities/is-name-unique`;
+      const config: any = { params: { name } };
 
-    if (oauth) {
-      config.headers = {
-        'X-OAuth-Token': JSON.stringify(oauth)
-      };
+      if (oauth) {
+        config.headers = {
+          "X-OAuth-Token": JSON.stringify(oauth),
+        };
+      }
+
+      const response = await this.axiosInstance.get(url, config);
+
+      if (response.data?.error) {
+        throw new APIError(
+          response.data.message,
+          response.data.code,
+          response.data.errors
+        );
+      }
+      return response.data;
+    } catch (error: any) {
+      if (error.response?.data) {
+        throw new APIError(
+          error.response.data.message,
+          error.response.status,
+          error.response.data.errors
+        );
+      }
+
+      throw new APIError(
+        "A problem was encountered during the request to check the charity name is unique.",
+        422
+      );
     }
-
-    const response = await this.axiosInstance.get(url, config);
-    return response.data;
   }
 }
