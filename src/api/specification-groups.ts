91f58f95c9d9d1f03aad79384a30a51a46dac7f7
Shubhh2903 <shubhamlilawala691@gmail.com>
2025-02-20 12:48:03 +0530

    task: change error handling in specification groups api

--- src/api/specification-groups.ts
+++ src/api/specification-groups.ts
@@ -1,4 +1,5 @@
 import { AuthService, TokenData } from "../services/auth";
+import { APIError } from "../types";
 import { BaseAPI } from "./base";
 
 export class SpecificationGroupsAPI extends BaseAPI {
@@ -13,17 +14,39 @@ export class SpecificationGroupsAPI extends BaseAPI {
    * @returns A list of specification groups
    */
   async list(params: {}, oauth: TokenData | null = null) {
-    const url = `${this.config.apiBasePath}/api/v1/specification-groups`;
-    const config: any = { params };
+    try {
+      const url = `${this.config.apiBasePath}/api/v1/specification-groups`;
+      const config: any = { params };
 
-    if (oauth) {
-      config.headers = {
-        "X-OAuth-Token": JSON.stringify(oauth),
-      };
+      if (oauth) {
+        config.headers = {
+          "X-OAuth-Token": JSON.stringify(oauth),
+        };
+      }
+
+      const response = await this.axiosInstance.get(url, config);
+      if (response.data?.error) {
+        throw new APIError(
+          response.data.message,
+          response.data.code,
+          response.data.errors
+        );
+      }
+      return response.data;
+    } catch (error: any) {
+      if (error.response?.data) {
+        throw new APIError(
+          error.response.data.message,
+          error.response.status,
+          error.response.data.errors
+        );
+      }
+
+      throw new APIError(
+        "A problem was encountered during the request to retrieve list of specification groups.",
+        422
+      );
     }
-
-    const response = await this.axiosInstance.get(url, config);
-    return response.data;
   }
 
   /**
@@ -34,16 +57,39 @@ export class SpecificationGroupsAPI extends BaseAPI {
    * @returns The specification group data
    */
   async get(id: string, params: {}, oauth: TokenData | null = null) {
-    const url = `${this.config.apiBasePath}/api/v1/specification-groups/${id}`;
-    const config: any = { params };
+    try {
+      const url = `${this.config.apiBasePath}/api/v1/specification-groups/${id}`;
+      const config: any = { params };
 
-    if (oauth) {
-      config.headers = {
-        "X-OAuth-Token": JSON.stringify(oauth),
-      };
+      if (oauth) {
+        config.headers = {
+          "X-OAuth-Token": JSON.stringify(oauth),
+        };
+      }
+
+      const response = await this.axiosInstance.get(url, config);
+      if (response.data?.error) {
+        throw new APIError(
+          response.data.message,
+          response.data.code,
+          response.data.errors
+        );
+      }
+
+      return response.data;
+    } catch (error: any) {
+      if (error.response?.data) {
+        throw new APIError(
+          error.response.data.message,
+          error.response.status,
+          error.response.data.errors
+        );
+      }
+
+      throw new APIError(
+        "A problem was encountered during the request to retrieve a specification group by id.",
+        422
+      );
     }
-
-    const response = await this.axiosInstance.get(url, config);
-    return response.data;
   }
 }
