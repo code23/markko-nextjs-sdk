aa30745c3938df45cbe3d464ff1c17cd49bba5fa
Shubhh2903 <shubhamlilawala691@gmail.com>
2025-02-20 18:19:16 +0530

    task: implement reference values api

---
+++ src/api/reference-values.ts
@@ -0,0 +1,54 @@
+import { AuthService, TokenData } from "../services/auth";
+import { APIError } from "../types";
+import { BaseAPI } from "./base";
+
+export class ReferenceValuesAPI extends BaseAPI {
+  constructor(config: any, authService: AuthService) {
+    super(config, authService);
+  }
+
+  /**
+   *Fetches reference values based on a given category.
+   *@param  params - Query parameters for filtering the lookup.
+   *@param  oauth - OAuth token data.
+   *@returns  - A promise resolving to a list of reference values.
+   */
+
+  async byCategory(params = {}, oauth: TokenData | null = null) {
+    try {
+      const url = `${this.config.apiBasePath}/api/v1/reference-values/lookup`;
+      const config: any = { params };
+
+      if (oauth) {
+        config.headers = {
+          "X-OAuth-Token": JSON.stringify(oauth),
+        };
+      }
+
+      const response = await this.axiosInstance.get(url, config);
+
+      if (response.data?.error) {
+        throw new APIError(
+          response.data.message,
+          response.data.code,
+          response.data.errors
+        );
+      }
+
+      return response.data.data ? response.data.data : [];
+    } catch (error: any) {
+      if (error.response?.data) {
+        throw new APIError(
+          error.response.data.message,
+          error.response.status,
+          error.response.data.errors
+        );
+      }
+
+      throw new APIError(
+        "A problem was encountered during the reference value lookup.",
+        422
+      );
+    }
+  }
+}
