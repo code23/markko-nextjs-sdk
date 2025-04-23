2d2bfd1aee41a2f0aade2009a2825fe2d268fda8
Barna Nagy <barna@meetmarkko.com>
2025-04-23 13:23:07 +0100

    fix arrays params, release 1.0.0-beta.23

--- src/api/base.ts
+++ src/api/base.ts
@@ -2,6 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 import https from 'https';
 import { MarkkoConfig } from '../types';
 import { AuthService } from '../services/auth';
+import qs from 'qs';
 
 export class BaseAPI {
   protected config: MarkkoConfig;
@@ -15,7 +16,8 @@ export class BaseAPI {
     const axiosConfig: any = {
       headers: {
         'X-MPE-Origin': this.config.origin
-      }
+      },
+      paramsSerializer: (params: any) => qs.stringify(params, { arrayFormat: 'brackets' })
     };
 
     // Only disable SSL verification in development
